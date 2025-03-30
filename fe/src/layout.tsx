f2ae3593339a1ccf5bf3440b41fbb02cb62c6b6f
julynguyen247 <julyneverdie@gmail.com>
2025-03-30 10:25:42 +0700

    react router v7

--- fe/src/layout.tsx
+++ fe/src/layout.tsx
@@ -1,11 +1,12 @@
-
-
+import { Outlet } from "react-router-dom"
+import AppHeader from "./components/layout/app.header"
 
 const Layout = () => {
   return (
-    <div>
-      loo
-    </div>
+    <>
+    <AppHeader/>
+    <Outlet/>
+    </>
   )
 }
 
--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,21 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Layout from './layout'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
-
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./styles/global.css"
+import ReactDOM from "react-dom/client";
+import Layout from "./layout";
+import LoginPage from "./pages/client/login";
+import HomePage from "./pages/client/home";
+import RegisterPage from "./pages/client/register";
+import { App } from "antd";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
+    Component: Layout,
+    children: [{ index: true, Component: HomePage }, {}],
+  },
+  {
+    path: "/login",
+    Component: LoginPage,
+  },
+  {
+    path: "/register",
+    Component: RegisterPage,
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+  <App>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </App>
+);
