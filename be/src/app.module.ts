67fa2f77a3f4b055a680bfb35e5745f3e244b902
julynguyen247 <julyneverdie@gmail.com>
2025-04-21 16:38:53 +0700

    response tempalte

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -7,6 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { ShoesModule } from './shoes/shoes.module';
 
 @Module({
   imports: [
@@ -25,6 +26,8 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
     }),
 
     AuthModule,
+
+    ShoesModule,
   ],
   controllers: [AppController],
   providers: [
--- be/src/main.ts
+++ be/src/main.ts
@@ -3,13 +3,23 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const reflector = app.get(Reflector);
-  // app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useGlobalPipes(new ValidationPipe());
+  //config cors
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  //config interceptor
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
   await app.listen(configService.get<string>('PORT') || 3000);
 }
 bootstrap();
